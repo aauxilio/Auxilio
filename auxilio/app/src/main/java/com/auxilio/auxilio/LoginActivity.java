a41b9aeb02e464a6c148cbd33759d80c6d602186
Jose Sanchez Garcia <josebsan96@gmail.com>
2018-08-12 01:04:33 -0700

    Changes needed to make requests

--- auxilio/app/src/main/java/com/auxilio/auxilio/LoginActivity.java
+++ auxilio/app/src/main/java/com/auxilio/auxilio/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.auxilio.auxilio;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,6 +14,22 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A login screen that offers login via email/password.
@@ -24,6 +42,58 @@ public class LoginActivity extends AppCompatActivity {
     private View mProgressView;
     private View mLoginFormView;
 
+    public static final String ACCOUNT_SID = "ACdf606dffe042a5ab003d42bad07d3784";
+    public static final String AUTH_TOKEN = "992b032ae2243a59c7ddf441ea135601";
+
+    private class Requests extends AsyncTask<URL, Integer, Long> {
+        protected Long doInBackground(URL... urls) {
+
+            String[] numbers = {"+15106480370", "+18188079879"};
+            for(String number : numbers){
+                sendSMS(number);
+            }
+            return new Long(1);
+        }
+
+        public void sendSMS(String number){
+            HttpClient httpclient = new DefaultHttpClient();
+
+            HttpPost httppost = new HttpPost(
+                    "https://api.twilio.com/2010-04-01/Accounts/"+ACCOUNT_SID+"/SMS/Messages");
+            String base64EncodedCredentials = "Basic "
+                    + Base64.encodeToString(
+                    (ACCOUNT_SID + ":" + AUTH_TOKEN).getBytes(),
+                    Base64.NO_WRAP);
+
+            httppost.setHeader("Authorization",
+                    base64EncodedCredentials);
+            try {
+
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("From",
+                        "+18312288938"));
+                nameValuePairs.add(new BasicNameValuePair("To",
+                        number));
+                nameValuePairs.add(new BasicNameValuePair("Body",
+                        "Welcome to Twilio"));
+
+                httppost.setEntity(new UrlEncodedFormEntity(
+                        nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                System.out.println("Entity post is: "
+                        + EntityUtils.toString(entity));
+
+
+            } catch (ClientProtocolException e) {
+
+            } catch (IOException e) {
+
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +111,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, PersonForm.class);
-                startActivity(intent);
-            }
-        });
+        new Requests().execute();
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
@@ -57,5 +120,7 @@ public class LoginActivity extends AppCompatActivity {
     private boolean isPasswordValid(String password) {
         return true;
     }
+
+
 }
 
