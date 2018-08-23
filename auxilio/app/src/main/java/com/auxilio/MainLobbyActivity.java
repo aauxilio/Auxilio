125f06af1dd94df5aeba9584e909aa65fe466b70
Dario Molina <dariomolina93>
2018-08-22 22:19:56 -0700

    Added sms functionality using SmsManager.

--- auxilio/app/src/main/java/com/auxilio/MainLobbyActivity.java
+++ auxilio/app/src/main/java/com/auxilio/MainLobbyActivity.java
@@ -1,24 +1,30 @@
 package com.auxilio;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.auxilio.auxilio.TwilioAPI;
+import com.auxilio.auxilio.SMS;
 import com.auxilio.data.AffidivitApplication;
 
 public class MainLobbyActivity extends AppCompatActivity {
 
     Button notifyRelatives;
     Button viewAffidivit;
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_lobby);
+        checkForPermissions();
 
         notifyRelatives = findViewById(R.id.notifi_relatives);
         viewAffidivit = findViewById(R.id.view_doc);
@@ -37,12 +43,34 @@ public class MainLobbyActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 AffidivitApplication application= DataUtils.getAffidivitApplication().build();
-                new TwilioAPI().execute(application);
-                Toast.makeText(MainLobbyActivity.this, "Parientes notificados", Toast.LENGTH_SHORT).show();
+//                new TwilioAPI().execute(application);
+//                Toast.makeText(MainLobbyActivity.this, "Parientes notificados", Toast.LENGTH_SHORT).show();
+
+                String sms = "This is going to be a really long message and it will take some time to get there,  This is going to be a really long message and it will take some time to get there";
+
+                SMS message = new SMS(getApplicationContext());
+                message.sendSMS("8317760235",  sms);
+
+
+
+
             }
         });
+    }
 
+    private void checkForPermissions()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.SEND_SMS)
+                    == PackageManager.PERMISSION_DENIED) {
 
+                Log.d("permission", "permission denied to SEND_SMS - requesting it");
+                String[] permissions = {Manifest.permission.SEND_SMS};
+
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+
+            }
+        }
     }
 
 }
